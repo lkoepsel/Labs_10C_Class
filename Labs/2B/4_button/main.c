fbac21cd28a4cac09981cc0de154535119492431
Lief Koepsel <lkoepsel@M1Mini.local>
2021-11-21 12:34:28 -0800

    Updated Lab 2B to match powerpoint.

--- Labs/2B/4_button/main.c
+++ Labs/2B/4_button/main.c
@@ -1,4 +1,4 @@
-/* button - the serial monitor will show the state of the button
+/* button - the serial monitor will show the state of the button */
 /* Put your #includes here */
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,23 +6,25 @@
 #include "uart.h"
 #include "pinMode.h"
 #include "digitalRead.h"
+#include "delay.h"
 
 /* Put your global variables here */
 
 /* Main */ 
 int main() {
 /* initialize code goes here */
-    init_serial;                
     int buttonState;
     int button = 10;
+
+    init_serial;
     pinMode(button, INPUT_PULLUP);
 
 // while(1) loop routine runs over and over again forever:
     while(1) {
-        /* execution code goes here */
         buttonState = digitalRead(button);
         printf("ButtonState is %d\n", buttonState);
-        delay(1000);      
+
+        delay(1000);
     }
     return(0);
 }
